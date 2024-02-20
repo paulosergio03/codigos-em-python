b8fb0b298debf72f43e6a509c546d6e5f2217f94
Paulo Sérgio <paulosergioadm9@gmail.com>
2024-02-20 18:06:53 -0300

    Modificação

--- Desafio014.py
+++
@@ -1,5 +0,0 @@
-#Conversor de temperatura de °C para °F
-
-c = float(input('Informe a temperatura em °C: '))
-f = ((9 * c) / 5) + 32
-print(f'A temperatura de {c}°C corresponde a {f}°F! ')
--- Desafio015.py
+++
@@ -1,4 +0,0 @@
-dias = float(input('Quantos dias? '))
-km = float(input('Quantos Km foram percorridos? '))
-pago = (dias * 60) + (km * 0.15)
-print(f'O total a pagar é de R${pago:.2f}')
